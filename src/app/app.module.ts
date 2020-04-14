059dc0a48aa5c13b69376487886000a4b39cd7b8
Alex <omareyes-96@hotmail.com>
2020-04-14 14:43:27 -0500

    integrando la plantilla adminlte al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/adminlte/header/header.component';
+import { MenuComponent } from './components/adminlte/menu/menu.component';
+import { ContentComponent } from './components/adminlte/content/content.component';
+import { FooterComponent } from './components/adminlte/footer/footer.component';
+import { SettingComponent } from './components/adminlte/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule
