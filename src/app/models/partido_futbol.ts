cfe730b34b6086ff8bc07c4382b38e18be8298d9
Alex <omareyes-96@hotmail.com>
2020-04-27 21:32:02 -0500

    metodos http para el servicio de partidos

---
+++ src/app/models/partido_futbol.ts
@@ -0,0 +1,8 @@
+export class PartidoFutbol{
+    constructor(
+        public id:number,
+        public fecha:Date,
+        public puntos:number,
+        public equipo_id:number
+    ){}
+}
---
+++ src/app/services/partido_futbol.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Global } from './global';
+
+@Injectable()
+export class PartidoFutbolService{
+    public url:string;
+    constructor(
+        private _http:HttpClient
+    ){
+        this.url=Global.url
+    }
+    //metodos http
+    getpartidos():Observable<any>{
+        return this._http.get(this.url +'partidos');
+    }
+    getpartido(id):Observable<any>{
+        return this._http.get(this.url +'partidos/'+id);
+    }
+    getequipos():Observable<any>{
+        return this._http.get(this.url+'equipos');
+    }
+    savepartido(partido):Observable<any>{
+        let headers=new HttpHeaders({'Content-Type':'application/json'});
+        return this._http.post(this.url + 'partidos',partido,{headers:headers})
+    }
+    updatepartido(partido,id):Observable<any>{
+        let headers=new HttpHeaders({'Content-Type':'application/json'});
+        return this._http.put(this.url + 'partidos/'+id,partido,{headers:headers})
+    }
+    deletepartido(id):Observable<any>{
+        return this._http.delete(this.url + 'partidos/d'+id);
+    }
+}
